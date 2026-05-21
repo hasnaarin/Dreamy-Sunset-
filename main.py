b593a464ec49a1298664aa9ba5ee4b89533318be
hasnaarin <hasnaarin78@gmail.com>
2026-05-21 13:30:10 +0700

    perbaikan backend dan sinkronisasi berkas

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # ══════════════════════════════════════
 @app.route('/')
 def index():
-    return render_template('keisanki.html')
+    return render_template('index.html')
 
 
 # ══════════════════════════════════════
@@ -293,4 +293,4 @@ def apply_binary(a, b, op):
 #  RUN
 # ══════════════════════════════════════
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
